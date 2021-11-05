7a3a8632f5ff8d49d54c837987adb7420b71513a
JakeFC <jacobchavera@yahoo.com>
2021-11-05 02:10:01 +0000

    rewrote without recursion

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
 """Module for canUnlockAll function"""
 
-import time
-
-unlock_tracker = []
-
 
 def canUnlockAll(boxes):
-    """Checks if all arrays can be unlocked inside matrix"""
-    if len(boxes) == 0:
-        return False
-    if type(boxes) is not list:
-        return False
-    global unlock_tracker
-    unlock_tracker = [False] * len(boxes)
-    unlock_tracker[0] = True
-    unlock(boxes, 0)
-    time.sleep(0.5)
-    if False in unlock_tracker:
-        return False
-    return True
-
-
-def unlock(boxes, box):
-    """Unlocks box at given index and each of its keys' boxes"""
-    if len(boxes[box]) == 0:
-        return
-    for i in boxes[box]:
-        if i < len(boxes) and unlock_tracker[i] is False:
-            unlock_tracker[i] = True
-            unlock(boxes, i)
+    """Determines if all boxes can be opened in matrix of key lists"""
+    unlocked = [0]
+    for box in unlocked:
+        for key in boxes[box]:
+            if key < len(boxes) and key not in unlocked:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
