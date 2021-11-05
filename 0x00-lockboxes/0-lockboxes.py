590ebf5187693d2a654ef13686429d4707a04e07
JakeFC <jacobchavera@yahoo.com>
2021-11-05 00:30:17 +0000

    task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module for canUnlockAll function"""
+
+unlock_tracker = []
+
+
+def canUnlockAll(boxes=[[]]):
+    """Checks if all arrays can be unlocked inside matrix"""
+    if (len(boxes) == 0):
+        return True
+    global unlock_tracker
+    unlock_tracker = [False] * len(boxes)
+    unlock_tracker[0] = True
+    Unlock(boxes, 0)
+    if (False in unlock_tracker):
+        return False
+    return True
+
+
+def Unlock(boxes=[[]], box=0):
+    """Unlocks box at given index and each of its keys' boxes"""
+    if (len(boxes[box]) == 0):
+        return
+    for i in boxes[box]:
+        if unlock_tracker[i] is False:
+            unlock_tracker[i] = True
+            Unlock(boxes, i)
