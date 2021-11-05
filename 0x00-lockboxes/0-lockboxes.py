1225e0e6c3ec2e1e68c8e82aad45836f58204f8f
JakeFC <jacobchavera@yahoo.com>
2021-11-05 02:04:34 +0000

    reverted global variable and added wait timer before checking if all boxes unlocked

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """Module for canUnlockAll function"""
 
+import time
+
+unlock_tracker = []
+
 
 def canUnlockAll(boxes):
     """Checks if all arrays can be unlocked inside matrix"""
@@ -8,19 +12,21 @@ def canUnlockAll(boxes):
         return False
     if type(boxes) is not list:
         return False
+    global unlock_tracker
     unlock_tracker = [False] * len(boxes)
     unlock_tracker[0] = True
-    unlock(boxes, 0, unlock_tracker)
+    unlock(boxes, 0)
+    time.sleep(0.5)
     if False in unlock_tracker:
         return False
     return True
 
 
-def unlock(boxes, box, unlock_tracker):
+def unlock(boxes, box):
     """Unlocks box at given index and each of its keys' boxes"""
     if len(boxes[box]) == 0:
         return
     for i in boxes[box]:
         if i < len(boxes) and unlock_tracker[i] is False:
             unlock_tracker[i] = True
-            unlock(boxes, i, unlock_tracker)
+            unlock(boxes, i)
