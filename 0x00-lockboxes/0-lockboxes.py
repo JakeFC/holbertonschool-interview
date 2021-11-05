cd160712064a5f85f501a856244525f72ca98f4e
JakeFC <jacobchavera@yahoo.com>
2021-11-05 00:47:13 +0000

    changed empty boxes variable check to return False

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -6,20 +6,20 @@ unlock_tracker = []
 
 def canUnlockAll(boxes=[[]]):
     """Checks if all arrays can be unlocked inside matrix"""
-    if (len(boxes) == 0):
-        return True
+    if len(boxes) == 0:
+        return False
     global unlock_tracker
     unlock_tracker = [False] * len(boxes)
     unlock_tracker[0] = True
     Unlock(boxes, 0)
-    if (False in unlock_tracker):
+    if False in unlock_tracker:
         return False
     return True
 
 
 def Unlock(boxes=[[]], box=0):
     """Unlocks box at given index and each of its keys' boxes"""
-    if (len(boxes[box]) == 0):
+    if len(boxes[box]) == 0:
         return
     for i in boxes[box]:
         if i < len(boxes) and unlock_tracker[i] is False:
