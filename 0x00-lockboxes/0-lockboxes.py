598a927984c20ddd412425dba47d503ec08e327d
JakeFC <jacobchavera@yahoo.com>
2021-11-05 01:50:43 +0000

    removed global variable

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Module for canUnlockAll function"""
 
-unlock_tracker = []
-
 
 def canUnlockAll(boxes):
     """Checks if all arrays can be unlocked inside matrix"""
@@ -10,20 +8,19 @@ def canUnlockAll(boxes):
         return False
     if type(boxes) is not list:
         return False
-    global unlock_tracker
     unlock_tracker = [False] * len(boxes)
     unlock_tracker[0] = True
-    Unlock(boxes, 0)
+    unlock(boxes, 0, unlock_tracker)
     if False in unlock_tracker:
         return False
     return True
 
 
-def Unlock(boxes, box):
+def unlock(boxes, box, unlock_tracker):
     """Unlocks box at given index and each of its keys' boxes"""
     if len(boxes[box]) == 0:
         return
     for i in boxes[box]:
         if i < len(boxes) and unlock_tracker[i] is False:
             unlock_tracker[i] = True
-            Unlock(boxes, i)
+            unlock(boxes, i, unlock_tracker)
