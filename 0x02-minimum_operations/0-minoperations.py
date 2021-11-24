c12e972e7c87d7cb11f7a0f1b71a005fca4cc550
JakeFC <jacobchavera@yahoo.com>
2021-11-24 20:25:12 +0000

    removed edge case and rewrote while dependency to account for it

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,12 +4,10 @@
 
 def minOperations(n):
     """Returns the minimum number of operations needed to print H n times"""
-    if (n <= 1):
-        return 0
     ops = 0
     m = 1
     copied = 0
-    while (m != n):
+    while (m < n):
         if (copied != m and n % m == 0):
             copied = m
         else:
