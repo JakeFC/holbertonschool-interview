651d38fadc0e0bcd395d3d9e5469f5a8f63a74a1
JakeFC <jacobchavera@yahoo.com>
2021-11-24 20:21:48 +0000

    rewrote without recursion, based on pattern

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,22 +4,15 @@
 
 def minOperations(n):
     """Returns the minimum number of operations needed to print H n times"""
-    if (n < 1):
+    if (n <= 1):
         return 0
-    if (n == 1):
-        return 1
-
-    def CopyAll(current):
-        """Copies the current number of H characters and runs Paste"""
-        return 1 + Paste(current, current)
-
-    def Paste(current, copied):
-        """Pastes the copied number of H characters and recursively runs
-        Paste or CopyAll"""
-        current += copied
-        if (current == n):
-            return 1
-        if (current > n):
-            return 0
-        return 1 + (CopyAll(current) or Paste(current, copied))
-    return CopyAll(1)
+    ops = 0
+    m = 1
+    copied = 0
+    while (m != n):
+        if (copied != m and n % m == 0):
+            copied = m
+        else:
+            m += copied
+        ops += 1    
+    return ops
