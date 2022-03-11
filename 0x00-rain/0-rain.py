58d105cb4386e6141114c32546c64e67f0a66b0e
JakeFC <jacobchavera@yahoo.com>
2022-03-11 08:01:15 +0000

    task 0

---
+++ 0x00-rain/0-rain.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module for rain function"""
+
+
+def rain(walls):
+    """Returns integer amount of rainwater retained by walls"""
+
+    water, left, spaces = 0, 0, 0
+
+    for n in walls:
+
+        if (n != 0):
+            """If this is a wall"""
+            if (left != 0):
+                """If left has already been set, add the number of spaces between walls
+                multiplied by the height of the lowest wall"""
+                water += (left * spaces) if left < n else (n * spaces)
+            """n is the height of the new left wall"""
+            left = n
+            spaces = 0
+
+        elif (left != 0):
+            spaces += 1
+
+    return water
